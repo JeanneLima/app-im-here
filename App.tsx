3978548d25d445491830d23485e7a95d90753c7e
Jeanne Lima <jeannelima.profissional@gmail.com>
2022-12-21 20:05:36 -0300

    refactor(header): refactor initial header styling with StyleSheet.creat

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-    }}>
-      <Text style={{
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>Nome do evento</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
       
-      <Text style={{
-        color: '#6B6B6B',
-        fontSize: 16
-      }}>Quarta, 21 de dezembro de 2022.</Text>
+      <Text style={styles.eventDate}>Quarta, 21 de dezembro de 2022.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  }
+})
