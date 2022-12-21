ff36af1757b3f675828dd63e83fc8a8443435ed4
Jeanne Lima <jeannelima.profissional@gmail.com>
2022-12-21 20:44:01 -0300

    feat(home): add structure, styling and initial trigger of add participant button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,11 @@
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from "./styles";
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar!")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -17,6 +21,15 @@ export default function Home() {
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={handleParticipantAdd}
+      >
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
