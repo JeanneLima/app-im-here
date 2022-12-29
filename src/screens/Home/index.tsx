1089555489f0e5d3cec98ea3066eb6c04336f314
Jeanne Lima <jeannelima.profissional@gmail.com>
2022-12-29 07:59:36 -0300

    feat(home): implement logic to remove participants from the list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -22,7 +22,7 @@ export function Home() {
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert('Deletado!')
+        onPress: () => setParticipants(previousState => previousState.filter(participant => participant !== name))
       },
       {
         text: 'Não',
